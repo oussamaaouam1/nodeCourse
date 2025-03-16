0a36876b46ce1ea38d5449dca04bbde8bb3f70f3
oussamaaouam1 <oussama.aouam@gmail.com>
2025-03-16 23:05:48 +0000

    changes

--- index.js
+++ index.js
@@ -6,28 +6,39 @@ const newItems = _.flatMapDeep(items);
 console.log(newItems);
 console.log("test");
 
-
-const { readFile} = require('fs')
-console.log('start the first task');
-readFile('./content/text.txt','utf8', (err,res)=>{
-
+const { readFile } = require("fs");
+console.log("start the first task");
+readFile("./content/text.txt", "utf8", (err, res) => {
   if (err) {
     console.log(err);
-    return
+    return;
   }
 
   console.log(res);
-  console.log('first task completed');
-})
-console.log('start next task');
-
+  console.log("first task completed");
+});
+console.log("start next task");
 
 console.log("first");
-setTimeout(()=>{
+setTimeout(() => {
   console.log("third");
-  
-},1 )
+}, 1);
 console.log("second");
 
+// console.log("start the task");
+// setInterval(() => {
+//   console.log("in task");
 
+// }, 2000);
+// console.log("finish the task");
 
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("hello node");
+  res.end("hello node");
+});
+
+server.listen(5000, () => {
+  console.log("server listening on 5000");
+});
